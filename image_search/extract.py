71609b6bce8854ee8a602a737e1d6ee3325a6d2e
pzarczynski <piotr.zarczynski.06@gmail.com>
2025-04-16 23:28:19 +0200

    fine-tune model and improve page filtering (caption regex)

--- image_search/extract.py
+++ image_search/extract.py
@@ -6,6 +6,7 @@ logger = logging.getLogger(__name__)
 import re
 
 import ultralytics
+import io
 
 import fitz
 import numpy as np
@@ -13,21 +14,13 @@ import requests
 from PIL import Image
 from tqdm import tqdm
 
-from pathlib import Path
-
 CAPTION_REGEX = re.compile(r"\b(fig(?:ure)?)\s*([\s\S]+)?", re.IGNORECASE)
 
-WEIGHTS_URL = (
-    "https://drive.google.com/uc?export=download&id=1T5zWVVgcymGKtkyYREQ0-cxmbnBMnatX"
-)
+WEIGHTS_URL = "https://drive.google.com/uc?export=download&id=12wkHnhD49uCoBIEUrJ_IVHpHutu0qNCe"
 
 
 def box_center(box):
     return (box[2] + box[0]) / 2, (box[3] + box[1]) / 2
-
-
-def box_centers(boxes):
-    return np.array([box_center(box) for box in boxes])
 
 
 def crop(image, box):
@@ -36,6 +29,7 @@ def crop(image, box):
 
 
 def distance_matrix(a, b, wx=1.0, wy=1.0, w_above=1.0):
+    a, b = np.array(a), np.array(b)
     dists = (a[:, None] - b[None, :]) ** 2
 
     dx = dists[..., 0] * wx
@@ -57,13 +51,11 @@ def render_page(page, dpi=100):
 
 class FigureExtractor:
     def __init__(self, weights_path="./data/weights/weights.pt"):
-        engine_path = os.path.splitext(weights_path)[0] + ".engine"
-
-        self.model = self._load_model(engine_path, weights_path)
+        self.model = self._load_model(weights_path)
 
     def _download_weights(self, path):
         logger.info("downloading weights...")
-        Path(os.path.dirname(path)).mkdir(parents=True, exist_ok=True)
+        os.makedirs(os.path.dirname(path), exist_ok=True)
 
         with requests.get(WEIGHTS_URL, stream=True) as r:
             r.raise_for_status()
@@ -76,85 +68,64 @@ class FigureExtractor:
                         f.write(chunk)
                         bar.update(len(chunk))
 
-    def _export_trt(self, model):
-        logger.info("exporting to tensorrt format...")
-        return model.export(format="engine", half=True)
-
-    def _load_model(self, engine_path, weights_path, fresh=False):
+    def _load_model(self, weights_path, fresh=False):
         """load model and download weights if not found"""
         if fresh:
             os.remove(weights_path)
-            os.remove(engine_path)
 
         try:
-            if not os.path.exists(engine_path):
-                if not os.path.exists(weights_path):
-                    self._download_weights(weights_path)
+            if not os.path.exists(weights_path):
+                self._download_weights(weights_path)
 
-                model = ultralytics.YOLO(weights_path)
-                # engine_path = self._export_trt(model)
+            model = ultralytics.YOLO(weights_path)
 
-        except RuntimeError:
-            return self._load_model(engine_path, weights_path, fresh=True)
+        except RuntimeError as e:
+            logger.warning(e)
+            logger.info("reloading model...")
+            return self._load_model(weights_path, fresh=True)
 
-        # model = ultralytics.YOLO(engine_path)
         return model
+
+    def _match_captions(self, render, captions, caption_centers, bboxes):
+        bboxes = [box.astype(int) for box in bboxes]
+
+        if bboxes:
+            bbox_centers = [box_center(box) for box in bboxes]
+
+            # x-axis distance is less important
+            # texts above the figure have an additional penalty
+            dists = distance_matrix(bbox_centers, caption_centers, wx=0.5, w_above=1.5)
+
+            indices = np.argmin(dists, axis=1)
+
+            return [
+                (crop(render, bbox), captions[idx])
+                for bbox, idx in zip(bboxes, indices)
+            ]
+
+        # return [(crop(render, box), None) for box in bboxes]
+        return []
 
     def __call__(self, pdf, **kwargs):
         out = []
-        
-        pdf = fitz.open(pdf) if isinstance(pdf, str) else fitz.open(stream=pdf)
+        pdf = fitz.open(stream=pdf) if isinstance(pdf, io.BytesIO) else fitz.open(pdf)
 
-        images = [render_page(p, dpi=100) for p in pdf]
-        results = self.model.predict(images, **kwargs)
+        all_captions = [
+            [b for b in page.get_text("blocks") if re.match(CAPTION_REGEX, b[4])]
+            for page in pdf
+        ]
 
-        for page, image, result in zip(pdf, images, results):
-            bboxes = [box.tolist() for box in result.boxes.xyxy]
+        renders = [render_page(p, dpi=100) for p, c in zip(pdf, all_captions) if c]
 
-            if bboxes:
-                bboxes = np.array(bboxes, dtype=int)
+        results = self.model.predict(renders, **kwargs)
 
-                captions = []
-                caption_centers = []
+        for render, captions, result in zip(
+            renders, filter(None, all_captions), results
+        ):
+            bboxes = [box.cpu().numpy() for box in result.boxes.xyxy]
+            captions = [c[4] for c in captions]
+            caption_centers = [box_center(c[:4]) for c in captions]
 
-                for block in page.get_text("blocks"):
-                    x1, y1, x2, y2, text, *_ = block
-                    center_x, center_y = x2 - x1, y2 - y1
-
-                    captions.append(text)
-                    caption_centers.append((center_x, center_y))
-
-                if captions:
-                    caption_centers = np.array(caption_centers)
-                    figure_centers = box_centers(bboxes)
-
-                    # x-axis distance is less important
-                    # texts above the figure should have an additional penalty
-                    dists = distance_matrix(
-                        figure_centers, caption_centers, wx=0.5, w_above=1.5
-                    )
-
-                    indices = np.argmin(dists, axis=1)
-
-                    for bbox, idx in zip(bboxes, indices):
-                        figure = crop(image, bbox)
-                        caption = captions[idx]
-
-                        out.append((figure, caption))
-
-                else:
-                    out.extend([(crop(image, bbox), None) for bbox in bboxes])
+            out.extend(self._match_captions(render, captions, caption_centers, bboxes))
 
         return out
-
-    def extract_from_dir(self, dir):
-        figures, captions = [], []
-
-        for file_name in os.listdir(dir):
-            path = os.path.join(dir, file_name)
-            new_figures, new_captions = zip(*self(path, verbose=False))
-
-            figures.extend(new_figures)
-            captions.extend(new_captions)
-
-        return figures, captions
